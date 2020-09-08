d16f6fa0af5a8b31068a35ca479661fdc184a775
Renato <renatoyufra@gmail.com>
2020-09-07 23:18:28 -0500

    [UI] Index page, Adding jsx, utils, theme

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,59 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import AppLayout from '../components/AppLayout'
+import { colors } from '../styles/theme'
+import Button from '../components/Button'
+import GitHub from '../components/Icons/GitHub'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>DebHub 🐹</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <AppLayout>
+        <section>
+          <img src="/devhub-logo.png" alt="Logo" />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <h1>DevHub</h1>
+          <h2>Talk about development <br /> with developers 🐱‍👤</h2>
+          <div>
+            <Button>
+              <GitHub width={24} height={24} fill='#fff' />
+              Login with GitHub
+            </Button>
+          </div>
+        </section>
+      </AppLayout>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <style jsx>{`
+        div {
+          margin-top: 16px;
+        }
+        img {
+          width: 120px;
+        }
+        
+        section {
+          display: grid;
+          height: 100%;
+          place-content: center;
+          place-items: center;
+        }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        h1 {
+          color: ${colors.secondary};
+          font-weight: 800;
+          margin-bottom: 16px;
+        }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        h2 {
+          color: ${colors.primary};
+          font-size: 21px;
+          margin: 0;
+        }
+      `}</style>
+    </>
   )
 }

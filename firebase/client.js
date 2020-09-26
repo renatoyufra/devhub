ba826750cf065d1b1143aa3111995c193f9bb862
Renato <renatoyufra@gmail.com>
2020-09-26 18:38:40 -0500

    Compose Devits and GetLastest Devits

--- firebase/client.js
+++ firebase/client.js
@@ -14,13 +14,17 @@ const firebaseConfig = {
 // si no se ha inicializado ninguna app => inicializamos
 !firebase.apps.length && firebase.initializeApp(firebaseConfig);
 
+// inicializar la BD firestore
+var db = firebase.firestore();
+
 const mapUserFromFirebaseAuthToUser = (user) => {
-  const { displayName, email, photoURL } = user;
+  const { displayName, email, photoURL, uid } = user;
 
   return {
     avatar: photoURL,
     username: displayName,
     email,
+    uid,
   };
 };
 
@@ -35,4 +39,39 @@ export const loginWithGithub = () => {
   const gitHubProvider = new firebase.auth.GithubAuthProvider();
 
   return firebase.auth().signInWithPopup(gitHubProvider);
+};
+
+export const addDevit = ({ avatar, content, userId, userName }) => {
+  return db.collection("devits").add({
+    avatar,
+    content,
+    userId,
+    userName,
+    createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+    likesCount: 0,
+    sharedCount: 0,
+  });
+};
+
+export const fetchLatestDevits = () => {
+  return db
+    .collection("devits")
+    .get()
+    .then(({ docs }) => {
+      return docs.map((doc) => {
+        const data = doc.data();
+        const id = doc.id;
+        const { createdAt } = data;
+        const date = new Date(createdAt.seconds * 1000);
+        const normalizedCreatedAt = new Intl.DateTimeFormat("es-ES").format(
+          date
+        );
+
+        return {
+          ...data,
+          id,
+          createdAt: normalizedCreatedAt,
+        };
+      });
+    });
 };
--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,7 @@ import AppLayout from "components/AppLayout";
 import { useEffect, useState } from "react";
 import Devit from "components/Devit";
 import useUser from "hooks/useUser";
+import { fetchLatestDevits } from "firebase/client";
 
 export default function HomePage() {
   const [timeline, setTimeline] = useState([]);
@@ -10,9 +11,9 @@ export default function HomePage() {
 
   useEffect(() => {
     user &&
-      fetch("http://localhost:3000/api/statuses/home_timeline")
-        .then((res) => res.json())
-        .then(setTimeline);
+      fetchLatestDevits().then((timeline) => {
+        setTimeline(timeline);
+      });
   }, [user]);
 
   return (
@@ -22,15 +23,19 @@ export default function HomePage() {
           <h2>Inicio</h2>
         </header>
         <section>
-          {timeline.map(({ id, avatar, username, message }) => (
-            <Devit
-              key={id}
-              avatar={avatar}
-              id={id}
-              message={message}
-              username={username}
-            />
-          ))}
+          {timeline.map(
+            ({ id, avatar, userName, content, userId, createdAt }) => (
+              <Devit
+                key={id}
+                avatar={avatar}
+                createdAt={createdAt}
+                id={id}
+                content={content}
+                userName={userName}
+                userId={userId}
+              />
+            )
+          )}
         </section>
         <nav></nav>
       </AppLayout>
@@ -43,7 +48,7 @@ export default function HomePage() {
             backdrop-filter: blur(5px);
             border-bottom: 1px solid #eee;
             height: 49px;
-            position: fixed;
+            position: sticky;
             top: 0;
             width: 100%;
         }
